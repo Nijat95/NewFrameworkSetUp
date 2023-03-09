e0ba88b05a37ec575e426ffb3bfca62a628278d8
Nijat <nganiyev@gmail.com>
2023-03-08 20:56:50 -0600

    Getting Error from Cukes Runner

---
+++ src/test/java/Runners/CukesRunner.java
@@ -0,0 +1,19 @@
+package Runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)// this line will force this class to run with Cucumber
+@CucumberOptions(
+
+        plugin = {"html:target/cucumber-report.html"},
+        features = "src/test/Resources/features",
+        glue = "step_def",
+        tags = "@postiveLoginSB",
+        dryRun = true
+
+)
+public class CukesRunner {
+
+}
---
+++ src/test/java/Utils/Config.java
@@ -0,0 +1,24 @@
+package Utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Config {
+
+    private static Properties properties = new Properties();
+    static{
+        String path = "Configuration.properties";
+        FileInputStream file;
+        try {
+            file = new FileInputStream(path);
+            properties.load(file);
+            file.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+}
