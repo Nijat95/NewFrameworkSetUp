c7fec6fae00521cadd184d3dc489d9a6139a4bde
Nijat <nganiyev@gmail.com>
2023-03-09 11:40:47 -0600

    Getting Error from Cukes Runner, also added Hooks, getting isFailed Method Error

---
+++ src/test/java/step_def/Hooks.java
@@ -0,0 +1,24 @@
+package step_def;
+
+import Utils.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+    @Before
+    public void setUp() {
+        System.out.println("The Scenario is running");
+    }
+    @After
+    public void tearDown(Scenario scenario) {
+        if(Scenario.isFailed() && Driver.getReference() != null){
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver())
+                    .getScreenshotAs(OutputType.BYTES);
+        scenario.attach(screenshot,"image/png","screenshot");
+    }
+        Driver.quitDriver();
+    }
+}
