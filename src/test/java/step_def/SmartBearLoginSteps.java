818407b13d337b2219b308d8e8c4b592be51bae4
Nijat <nganiyev@gmail.com>
2023-03-10 17:33:22 -0600

    Framework Setup and working

--- src/test/java/step_def/SmartBearLoginSteps.java
+++ src/test/java/step_def/SmartBearLoginSteps.java
@@ -1,4 +1,27 @@
 package step_def;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class SmartBearLoginSteps {
+    @When("User is on SmartBear login Page")
+    public void user_is_on_smart_bear_login_page() {
+
+    }
+    @When("User enters correct username")
+    public void user_enters_correct_username() {
+
+    }
+    @When("Enters the correct password")
+    public void enters_the_correct_password() {
+
+    }
+    @When("User clicks to login button")
+    public void user_clicks_to_login_button() {
+
+    }
+    @Then("User should ne logged in and be able to verify Welcome text is displayed")
+    public void user_should_ne_logged_in_and_be_able_to_verify_welcome_text_is_displayed() {
+
+    }
 }
---
+++ src/test/java/step_def/SmartBearSteps.java
@@ -0,0 +1,8 @@
+package step_def;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SmartBearSteps {
+
+}
